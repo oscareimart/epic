7111ac8358744aaef0c0b80b5f0495a4831a6241
oscareimart <115655099+oscareimart@users.noreply.github.com>
2023-04-03 16:59:34 -0400

    Update Sponsors.jsx

--- components/footer/Sponsors.jsx
+++ components/footer/Sponsors.jsx
@@ -5,7 +5,7 @@ const Sponsors = (props) => {
         <>
             <div className="container mb-5">
                 <div className="row">
-                    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4 d-flex justify-content-center">
+                    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12 d-flex justify-content-center">
                         <Image
                             src="https://web.epicbolivia.travel/tripadvisor_epic_1.webp"
                             width={150}
@@ -14,7 +14,7 @@ const Sponsors = (props) => {
                             className="img-fluid"
                         />
                     </div>
-                    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4 d-flex justify-content-center">
+                    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12 d-flex justify-content-center">
                         <Image
                             src="https://web.epicbolivia.travel/europassistance_epic.webp"
                             width={150}
@@ -23,7 +23,7 @@ const Sponsors = (props) => {
                             className="img-fluid"
                         />
                     </div>
-                    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4 d-flex justify-content-center">
+                    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12 d-flex justify-content-center">
                         <Image
                             src="https://web.epicbolivia.travel/marcapais_epic.webp"
                             width={150}
