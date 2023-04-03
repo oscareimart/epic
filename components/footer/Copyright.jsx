7e6441b18fcee757ef18ed4281ac1afec5ed6af6
oscareimart <115655099+oscareimart@users.noreply.github.com>
2023-04-03 16:55:28 -0400

    Update Copyright.jsx

--- components/footer/Copyright.jsx
+++ components/footer/Copyright.jsx
@@ -9,28 +9,34 @@ const Copyright = (props) => {
             <div
                 className="bg-black d-inline-block copyright-container"
             >
-                <label className="pt-3 copyright_logo">
-                    <Link href="/">
-                        {dataCompany.state?.imagen_pie_pagina?.data?.attributes?.url && <Image
-                            src={`${env_values.URL_BACKEND}${dataCompany.state?.imagen_pie_pagina?.data?.attributes?.url}`}
-                            width={150}
-                            height={38}
-                            alt="FooterImg"
-                        />}
+                <div className="row">
+                    <div className="col-md-6">
+                        <label className="pt-3 copyright_logo">
+                            <Link href="/">
+                                {dataCompany.state?.imagen_pie_pagina?.data?.attributes?.url && <Image
+                                    src={`${env_values.URL_BACKEND}${dataCompany.state?.imagen_pie_pagina?.data?.attributes?.url}`}
+                                    width={150}
+                                    height={38}
+                                    alt="FooterImg"
+                                />}
 
-                    </Link>
-                </label>
-                <p className='text-white pt-4'>
-                    © 2022 Epic Travel Bolivia . All Rights Reserved | Design by&nbsp;
-                    <a
-                        href="https://comtech.com.bo/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className='text-decoration-none text-danger'
-                    >
-                        Comtech
-                    </a>
-                </p>
+                            </Link>
+                        </label>
+                    </div>
+                    <div className="col-md-6">
+                        <p className='text-white pt-4'>
+                            © 2022 Epic Travel Bolivia . All Rights Reserved | Design by&nbsp;
+                            <a
+                                href="https://comtech.com.bo/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className='text-decoration-none text-danger'
+                            >
+                                Comtech
+                            </a>
+                        </p>
+                    </div>
+                </div>
             </div>
         </>
     )
